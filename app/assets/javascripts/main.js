08d944f3554000c7e9a0a00f5f1b511673d02771
zumbalogy <intrapostal@gmail.com>
2014-04-04 19:54:23 -0400

    tells time in letters for break

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,7 @@
-var count = 0
-var pause = false
+var count = 1195
+var pause = false;
 
-var audio = new Audio('alert.mp3')
+var audio = new Audio('alert.mp3');
 
 function tick(){
     count += 1
@@ -9,9 +9,11 @@ function tick(){
         var break_count = count % 1200
         var break_width = 100 - ((break_count / 20) * 100)
         $('#break-bar').width("" + break_width + '%')
+         $('#break-words').text('Break over in: ' + (20 - break_count) + 's')
         if (count > 1220) {
             count = 0
             break_count = 0
+            $('#break-words').text('Break over in: ')
         }
     }
     var width = 100 - ((count / 1200) * 100)
