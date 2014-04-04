4f37f7aa1e77f213f5f6fdcdf6684c418617b0fd
zumbalogy <intrapostal@gmail.com>
2014-04-04 19:27:53 -0400

    bars work together and all

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,18 @@
-var count = 0
+var count = 1190
 var pause = false;
 
 
 
 function tick(){
-    count++
-    if (count === 1200) {
-        count = 0
+    count += 2
+    if (count > 1200) {
+        var break_count = count % 1200
+        var break_width = 100 - ((break_count / 20) * 100)
+        $('#break-bar').width("" + break_width + '%')
+        if (count > 1220) {
+            count = 0
+            break_count = 0
+        }
     }
     var width = 100 - ((count / 1200) * 100)
     $('#work-bar').width("" + width + '%')
