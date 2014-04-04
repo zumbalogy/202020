9a8995ceb9a2e783909ac0d9560e7eaebac5b96f
zumbalogy <intrapostal@gmail.com>
2014-04-04 18:02:32 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twentytwenty::Application.config.secret_key_base = 'f6a38aa140a37820d6d0fdd1b8801164513f01ad6511c3a4ded7f3652ce621d8de3559d5969ecfdd0bd948ded9188b474c5225a9e6e12ca7751f5d0c115cace7'
