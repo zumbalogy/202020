6b4bdb6dbc776594146c613350414031b19548bf
zumbalogy <intrapostal@gmail.com>
2014-04-04 20:24:40 -0400

    style and pause button

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,4 @@
-var count = 1195
+var count = 0
 var pause = false;
 
 var audio = new Audio('alert.mp3');
@@ -6,18 +6,27 @@ var audio = new Audio('alert.mp3');
 function tick(){
     count += 1
     if (count > 1200) {
+        $('#next-words').text('Next Break in: ')
         var break_count = count % 1200
         var break_width = 100 - ((break_count / 20) * 100)
         $('#break-bar').width("" + break_width + '%')
-         $('#break-words').text('Break over in: ' + (20 - break_count) + 's')
+        $('#break-words').text('Break Over in: ' + (20 - break_count) + 's')
         if (count > 1220) {
             count = 0
             break_count = 0
-            $('#break-words').text('Break over in: ')
+            $('#break-words').text('Break Over in: ')
         }
+    } else {
+        var width = 100 - ((count / 1200) * 100)
+        $('#work-bar').width("" + width + '%')
+        var mins = Math.floor(20 - (count / 60))
+        if (count === 1200) {
+            var secs = 0
+        } else {
+            var secs = 60 - (count % 60)
+        }
+        $('#next-words').text('Next Break in: ' + mins + 'm ' + secs + 's')
     }
-    var width = 100 - ((count / 1200) * 100)
-    $('#work-bar').width("" + width + '%')
 }
 
 setInterval(function(){
@@ -28,3 +37,16 @@ setInterval(function(){
         }
     }
 },1000)
+
+
+$(function(){
+    var button = $('#pause')
+    button.on('click', function(){
+        pause = !pause
+        if (pause === true) {
+            button.text('Unpause')
+        } else {
+             button.text('Pause')
+        }
+    })
+})
